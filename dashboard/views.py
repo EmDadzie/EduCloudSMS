2b9973a133fe1a88ec3831119c3e36fa1364dfb4
EmDadzie <edadzie93@gmail.com>
2026-07-29 00:58:20 +0100

    Complete dashboard layout and foundation

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 from students.models import Student
 from teachers.models import Teacher
@@ -7,14 +6,22 @@ from classes.models import ClassRoom
 from subjects.models import Subject
 
 
-@login_required
 def home(request):
 
     context = {
-        "student_count": Student.objects.count(),
-        "teacher_count": Teacher.objects.count(),
-        "class_count": ClassRoom.objects.count(),
-        "subject_count": Subject.objects.count(),
+
+        "total_students": Student.objects.count(),
+
+        "total_teachers": Teacher.objects.count(),
+
+        "total_classes": ClassRoom.objects.count(),
+
+        "total_subjects": Subject.objects.count(),
+
     }
 
-    return render(request, "dashboard/home.html", context)
+    return render(
+        request,
+        "dashboard/home.html",
+        context
+    )
